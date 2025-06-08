9b12ae0c1171a4cd42200a72c57c0ee3009ee4aa
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-06-08 21:29:46 +0530

    Add ionic config to run  in mobile phone

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'http://192.168.1.8:8100',
+    cleartext: true
+  }
 };
 
 export default config;
--- src/app/welcomescreen/welcomescreen.component.ts
+++ src/app/welcomescreen/welcomescreen.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-welcomescreen',
   templateUrl: './welcomescreen.component.html',
   styleUrls: ['./welcomescreen.component.scss'],
   standalone: true,
+  imports: [IonContent,]
 })
-export class WelcomescreenComponent  implements OnInit {
+export class WelcomescreenComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
